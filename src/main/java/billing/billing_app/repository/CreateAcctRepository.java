116145a73506e322ade3f001d78b3b650a925ab5
Ayush Chaudhary <gautam617@gmail.com>
2025-01-22 13:07:38 -0500

    new update

--- src/main/java/billing/billing_app/repository/CreateAcctRepository.java
+++ src/main/java/billing/billing_app/repository/CreateAcctRepository.java
@@ -2,10 +2,14 @@ package billing.billing_app.repository;
 
 import billing.billing_app.model.BillingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface CreateAcctRepository extends JpaRepository<BillingEntity, Long> {
 
-    Optional<BillingEntity> findByEmail(String email);
+    @Query("SELECT b FROM BillingEntity b WHERE b.user.email = :email")
+    Optional<BillingEntity> findByEmail(@Param("email") String email);
+
 }
